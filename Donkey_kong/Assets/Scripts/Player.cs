7f752b8f2bcd45c40a09fa3bb5e9870e9bc535f3
Arabi11 <93090835+Arabi11@users.noreply.github.com>
2022-02-11 05:35:32 +0000

    Added directional change to left and right movement

--- Donkey_kong/Assets/Scripts/Player.cs
+++ Donkey_kong/Assets/Scripts/Player.cs
@@ -2,9 +2,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   private new Rigidbody2D rigidbody;
+    private new Rigidbody2D rigidbody;
+    private Vector2 direction;
+    public float moveSpeed = 1f;
 
-   private void Awake(){
-      rigidbody = GetComponent<Rigidbody2D>();
-   }
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal") * moveSpeed;
+        //direction.y = Input.GetAxis("Vertical") * moveSpeed;
+
+        if (direction.x > 0f)
+        {
+            transform.eulerAngles = Vector3.zero;
+        } else if (direction.x < 0f){
+           transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + direction * Time.fixedDeltaTime);
+    }
 }
