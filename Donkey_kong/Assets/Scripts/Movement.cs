4689a06b29f6f75113637a7d095d7532503e96d7
Arabi11 <93090835+Arabi11@users.noreply.github.com>
2022-04-13 07:43:28 +0100

    added grounding and ground layer

--- Donkey_kong/Assets/Scripts/Movement.cs
+++ Donkey_kong/Assets/Scripts/Movement.cs
@@ -6,16 +6,45 @@ public class Movement : MonoBehaviour
     private Vector2 direction;
     public float moveSpeed = 1f;
     public float jumpStrength =1f;
+    private new Collider2D collider;
+
+
+    private bool grounded;
+    private Collider2D [] results;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        results = new Collider2D[4];
         
     }
+    public void CheckCollision(){
+
+        grounded = false;
+        Vector2 size = collider.bounds.size;
+        size.y += 0.1f;
+        size.x /= 2f;
+        int amount = Physics2D.OverlapBoxNonAlloc(transform.position, size, 0f, results);
+
+        for(int i= 0; i < amount; i++){
+           
+           GameObject hit  = results[i].gameObject;
+
+             if (hit.layer == LayerMask.NameToLayer("Ground")){
+                 grounded = hit.transform.position.y < (transform.position.y - 0.5f);
+
+                 Physics2D.IgnoreCollision(collider, results[i], !grounded);
+            }
+        }
+    }
+
 
     private void Update()
     {
-        if (Input.GetButtonDown("Jump")){
+
+        CheckCollision();
+        if (grounded && Input.GetButtonDown("Jump")){
            direction = Vector2.up * jumpStrength;
         }
         else{
@@ -23,7 +52,10 @@ public class Movement : MonoBehaviour
         }
 
         direction.x = Input.GetAxis("Horizontal") * moveSpeed;
-        direction.y = Mathf.Max(direction.y,-1f);
+        if(grounded){
+            direction.y = Mathf.Max(direction.y,-1f);
+        }
+        
 
         if (direction.x > 0f)
         {
