ba07721a3ed82fa09b17359e7e07161dc0e289b2
Arabi11 <93090835+Arabi11@users.noreply.github.com>
2022-04-15 00:49:43 +0100

    Added Animation to mario

--- Donkey_kong/Assets/Scripts/Movement.cs
+++ Donkey_kong/Assets/Scripts/Movement.cs
@@ -2,73 +2,127 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    
+
+    [SerializeField] private Sprite[] runSprites;
+    [SerializeField] private  float moveSpeed = 3f;
+
+    [SerializeField] private Sprite climbSprite;
+
+    [SerializeField] private float jumpStrength = 4f;
+    
+    private SpriteRenderer spriteRenderer;
+    
+   
+    private int spriteIndex;
+
     private new Rigidbody2D rigidbody;
-    private Vector2 direction;
-    public float moveSpeed = 1f;
-    public float jumpStrength =1f;
     private new Collider2D collider;
 
+    private Collider2D[] overlaps = new Collider2D[4];
+    private Vector2 direction;
 
     private bool grounded;
-    private Collider2D [] results;
+    private bool climbing;
+
+    
+    
 
     private void Awake()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         rigidbody = GetComponent<Rigidbody2D>();
         collider = GetComponent<Collider2D>();
-        results = new Collider2D[4];
-        
-    }
-    public void CheckCollision(){
-
-        grounded = false;
-        Vector2 size = collider.bounds.size;
-        size.y += 0.1f;
-        size.x /= 2f;
-        int amount = Physics2D.OverlapBoxNonAlloc(transform.position, size, 0f, results);
-
-        for(int i= 0; i < amount; i++){
-           
-           GameObject hit  = results[i].gameObject;
-
-             if (hit.layer == LayerMask.NameToLayer("Ground")){
-                 grounded = hit.transform.position.y < (transform.position.y - 0.5f);
-
-                 Physics2D.IgnoreCollision(collider, results[i], !grounded);
-            }
-        }
     }
 
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 1f/12f, 1f/12f);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
 
     private void Update()
     {
-
         CheckCollision();
-        if (grounded && Input.GetButtonDown("Jump")){
-           direction = Vector2.up * jumpStrength;
+        SetDirection();
+    }
+
+    private void CheckCollision()
+    {
+        grounded = false;
+        climbing = false;
+
+        Vector3 size = collider.bounds.size;
+        size.y += 0.1f;
+        size.x /= 2f;
+
+        int amount = Physics2D.OverlapBoxNonAlloc(transform.position, size, 0, overlaps);
+
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject hit = overlaps[i].gameObject;
+
+            if (hit.layer == LayerMask.NameToLayer("Ground"))
+            {
+                // Only set as grounded if the platform is below the player
+                grounded = hit.transform.position.y < (transform.position.y - 0.5f);
+
+                // Turn off collision on platforms the player is not grounded to
+                Physics2D.IgnoreCollision(overlaps[i], collider, !grounded);
+            }
+           
+           
         }
-        else{
-            direction += Physics2D.gravity * Time.deltaTime;
+    }
+
+    private void SetDirection()
+    {
+        if (climbing) {
+            direction.y = Input.GetAxis("Vertical") * moveSpeed;
+        } else if (grounded && Input.GetButtonDown("Jump")) {
+            direction = Vector2.up * jumpStrength;
+        } else {
+            direction += Physics2D.gravity * Time.deltaTime*2;
         }
 
         direction.x = Input.GetAxis("Horizontal") * moveSpeed;
-        if(grounded){
-            direction.y = Mathf.Max(direction.y,-1f);
-        }
-        
 
-        if (direction.x > 0f)
-        {
+        // Prevent gravity from building up infinitely
+        if (grounded) {
+            direction.y = Mathf.Max(direction.y, -1f);
+        }
+
+        if (direction.x > 0f) {
             transform.eulerAngles = Vector3.zero;
-        } else if (direction.x < 0f){
-           transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        } else if (direction.x < 0f) {
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
         }
-
-
     }
 
     private void FixedUpdate()
     {
         rigidbody.MovePosition(rigidbody.position + direction * Time.fixedDeltaTime);
+    }
+
+    private void AnimateSprite()
+    {
+        if (direction.x == 0f)
+        {
+            spriteRenderer.sprite = runSprites[0];
+        }
+        else if (direction.x != 0f)
+        {
+            spriteIndex++;
+
+            if (spriteIndex >= runSprites.Length) {
+                spriteIndex = 0;
+            }
+
+            spriteRenderer.sprite = runSprites[spriteIndex];
+        }
     }
 }
